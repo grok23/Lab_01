76aa31dc4bcce2055f8f2000b18d7f56c84ed73d
grok23 <s00189752@mail.itsligo.ie>
2020-10-07 14:42:52 +0100

    added if/else check for blank name field and appropriate toast messages

--- app/src/main/java/edu/amacdonald/lab_01/MainActivity.java
+++ app/src/main/java/edu/amacdonald/lab_01/MainActivity.java
@@ -1,21 +1,17 @@
 package edu.amacdonald.lab_01;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
 
     EditText usersName = null;
     ImageView image;
     ImageView image2;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,32 +20,38 @@ public class MainActivity extends AppCompatActivity {
 
         //links Java usersName to XML component
         usersName = findViewById(R.id.editTextTextPersonName);
-
     }
 
     public void doName(View view) {
         String name = null;
         name = usersName.getText().toString();
-        //change the image in imageView2 from background space to "not imposter" pic
-        image = (ImageView) findViewById(R.id.imageView2);
-        image.setImageResource(R.drawable.notimposterbackground1);
 
-        Toast toast= Toast.makeText(getApplicationContext(),
-                name + ", was not the impostor!", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP, 0, 0);
-        toast.show();
+        /* if/else checks for empty name field and delivers appropriate toast*/
+        if (name.matches("")) {
+            Toast.makeText(this, "Not suspicious of anybody? That's sus!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else {
+            //change the image in imageView2 from background space to "not impostor" pic
+            image = (ImageView) findViewById(R.id.imageView2);
+            image.setImageResource(R.drawable.notimposterbackground1);
+            //Insert name from text entry box and use in Toast message.
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    name + ", was not the impostor!", Toast.LENGTH_LONG);
+            toast.show();
 
-        new CountDownTimer(4000, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                // millisUntilFinished    The amount of time until finished.
-            }
-            public void onFinish() {
-                image2 = (ImageView) findViewById(R.id.imageView2);
-                image2.setImageResource(R.drawable.spacebackground1);
-                usersName.getText().clear();
-            }
-        }.start();
-
+            new CountDownTimer(4000, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    // millisUntilFinished    The amount of time until finished. (without the onTick the timer will not run)
+                }
+                //at finish of countdown change image back to spacebaground1 and clear the text box
+                public void onFinish() {
+                    image2 = (ImageView) findViewById(R.id.imageView2);
+                    image2.setImageResource(R.drawable.spacebackground1);
+                    usersName.getText().clear();
+                }
+            }.start();
+        }
     }
 }
